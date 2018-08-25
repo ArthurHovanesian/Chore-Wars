800aa57421e84397245312f0d2ad3cc121e0fb8e
Arthur Hovanesian <nameonascreen2@gmail.com>
2018-08-25 15:42:28 -0700

    Add text and input boxes to modal

--- client/src/SignIn/SignIn.jsx
+++ client/src/SignIn/SignIn.jsx
@@ -10,6 +10,35 @@ const SignIn = ({ toggleSignIn }) => {
   return (
     <div className={styles.outer} onClick={() => toggleSignIn()}>
       <div className={styles.inner} onClick={stopProp}>
+        <div>
+          <div>
+            <input type="text" className={styles.userInput}/>
+            <span>Username</span>
+          </div>
+          <div>
+            <input type="text" className={styles.userInput}/>
+              <span>Password</span>
+          </div>
+          <button type="button">Sign In</button>
+          <div className={styles.notMember}> Not a member? Sign Up Below!</div>
+          <div>
+            <input type="text" className={styles.userInput}/>
+            <span>Email</span>
+          </div>
+          <div>
+            <input type="text" className={styles.userInput}/>
+            <span>Username</span>
+          </div>
+          <div>
+            <input type="text" className={styles.userInput}/>
+              <span>Password</span>
+          </div>
+          <div>
+            <input type="text" className={styles.userInput}/>
+            <span>Retype Password</span>
+          </div>
+          <button type="button">Register</button>
+        </div>
       </div>
     </div>
   )
